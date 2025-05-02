afd6a5e84e8a677ddecc5a7df2fabb70d538c3ad
MichalM26 <michal.milerowski@gmail.com>
2025-05-02 16:16:19 +0200

    Add files via upload

--- calculator_banks.py
+++ calculator_banks.py
@@ -52,3 +52,19 @@ if st.session_state.authenticated:
             st.info("Zdolność umiarkowana")
         else:
             st.success("Zdolność wysoka – potencjalna zdolność hipoteczna")
+
+        st.subheader("🏦 Przykładowe oferty kredytów gotówkowych (maj 2025)")
+        banki = [
+            {"Bank": "Citi Handlowy", "Kwota": "150 000 zł", "RRSO": "10,35%", "Oprocentowanie": "9,89%", "Prowizja": "0%"},
+            {"Bank": "Alior Bank", "Kwota": "250 000 zł", "RRSO": "10,36%", "Oprocentowanie": "9,90%", "Prowizja": "0%"},
+            {"Bank": "BNP Paribas", "Kwota": "230 000 zł", "RRSO": "10,43%", "Oprocentowanie": "9,95%", "Prowizja": "0%"},
+            {"Bank": "Santander Consumer Bank", "Kwota": "300 000 zł", "RRSO": "10,46%", "Oprocentowanie": "9,99%", "Prowizja": "0%"},
+            {"Bank": "Bank Pekao", "Kwota": "250 000 zł", "RRSO": "10,99%", "Oprocentowanie": "10,47%", "Prowizja": "0%"},
+            {"Bank": "VeloBank", "Kwota": "300 000 zł", "RRSO": "11,41%", "Oprocentowanie": "10,85%", "Prowizja": "0%"},
+            {"Bank": "Kasa Stefczyka", "Kwota": "100 000 zł", "RRSO": "11,46%", "Oprocentowanie": "10,90%", "Prowizja": "0%"},
+            {"Bank": "PKO BP", "Kwota": "300 000 zł", "RRSO": "11,56%", "Oprocentowanie": "10,99%", "Prowizja": "0%"},
+            {"Bank": "Raiffeisen Digital Bank", "Kwota": "150 000 zł", "RRSO": "11,99%", "Oprocentowanie": "11,38%", "Prowizja": "0%"},
+            {"Bank": "Santander Bank Polska", "Kwota": "300 000 zł", "RRSO": "12,67%", "Oprocentowanie": "11,99%", "Prowizja": "0%"},
+        ]
+        df_banki = pd.DataFrame(banki)
+        st.dataframe(df_banki, use_container_width=True)
